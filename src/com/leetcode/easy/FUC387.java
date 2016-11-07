e4f9e93339c9f4462678ef04b1ad91206751f580
pro <lioo45@qq.com>
2016-11-07 19:16:04 +0800

    first commit

--- src/com/leetcode/easy/FUC387.java
+++ src/com/leetcode/easy/FUC387.java
@@ -1,10 +1,34 @@
 package com.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by pro on 16/11/5.
  */
 public class FUC387 {
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("loveleetcode"));
+    }
     public static int firstUniqChar(String s) {
+        Map<Character,Integer> hash=new HashMap<Character, Integer>();
+        char c[]=s.toCharArray();
+        for(char cc:c){
+            Integer i=hash.get(cc);
+            if(i==null)
+                hash.put(cc,1);
+            else{
+                ++i;
+                hash.put(cc,i);
+            }
+        }
+        for (int i = 0; i < c.length; i++) {
+            if(hash.get(c[i])<2)
+                return i;
+        }
 
+        return -1;
     }
 }
